fa8089e2c969b3456660c59d4249a4f167ca8cc8
Charles Carvalho <charlesscarvalhoo@gmail.com>
2024-08-23 20:02:11 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criando um projeto completo, baseado no tab-new de Filipe
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home(){
-    return <h1>Tati, Eu te amo muito! Se você me ama, 
-        dê uma risadinha :P
-    </h1>
+function Home() {
+  return <h1>Tati, Eu te amo muito! Se você me ama, dê uma risadinha :P</h1>;
 }
 
 export default Home;
